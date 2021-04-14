4121b2ce1301bae993f80e035671072241074580
mohsen.mhbi <mohsen.mhbi@gmail.com>
2021-04-14 20:26:24 +0430

    Add project files.

---
+++ CustomConfiguration/ConfigurationProviders/EFConfigurationSource.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace CustomConfiguration
+{
+    public class EFConfigurationSource : IConfigurationSource
+    {
+        private readonly Action<DbContextOptionsBuilder> _optionsAction;
+        public EFConfigurationSource(Action<DbContextOptionsBuilder> optionsAction)
+        {
+            _optionsAction = optionsAction;
+        }
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return new EFConfigurationProvider(_optionsAction);
+        }
+    }
+}
---
+++ CustomConfiguration/Context/ConfigurationEntityConfig.cs
@@ -0,0 +1,14 @@
+﻿using CustomConfiguration.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CustomConfiguration.Context
+{
+    class ConfigurationEntityConfig : IEntityTypeConfiguration<ConfigurationEntity>
+    {
+        public void Configure(EntityTypeBuilder<ConfigurationEntity> builder)
+        {
+            builder.HasKey(c => c.Key);
+        }
+    }
+}
